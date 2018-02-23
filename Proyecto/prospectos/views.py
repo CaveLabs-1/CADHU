6f02530f8f012bd613a5ad905a94cd25687c77a0
lunamrcc <A01209537@itesm.mx>
2018-02-23 13:23:16 -0600

    Update views.py

--- Proyecto/prospectos/views.py
+++ Proyecto/prospectos/views.py
@@ -5,31 +5,31 @@ from .models import Prospecto, Lugar
 from .forms import ProspectoForm, LugarForm
 
 # Create your views here.
-def prospecto_crear(request):
-    context = {}
-    NewProspectoForm = ProspectoForm()
-    NewLugarForm = LugarForm()
-
-    if request.method == 'POST':
-        NewProspectoForm = ProspectoForm(request.POST)
-        NewLugarForm = LugarForm(request.POST)
-        ProspectoValid = NewProspectoForm.is_valid()
-        LugarValid = NewLugarForm.is_valid()
-
-        if ProspectoValid and LugarValid:
-            Lugar = NewLugarForm.save()
-            Prospecto = NewProspectoForm.save(commit = False)
-            Prospecto.Direccion = Lugar
-            Prospecto.save()
-            return prospecto_lista(request)
-
-    context = {
-        'NewProspectoForm' = NewProspectoForm,
-        'NewLugarForm' = NewLugarForm,
-    }
-    return render(request, 'prospecto/prospecto_form.html', context)
-
-def prospecto_lista(request):
-    context = {}
-    return render(request, 'prospecto/prospecto_lista.html',context)
-
+#def prospecto_crear(request):
+#    context = {}
+#    NewProspectoForm = ProspectoForm()
+#    NewLugarForm = LugarForm()
+#
+#   if request.method == 'POST':
+#        NewProspectoForm = ProspectoForm(request.POST)
+#        NewLugarForm = LugarForm(request.POST)
+#        ProspectoValid = NewProspectoForm.is_valid()
+#        LugarValid = NewLugarForm.is_valid()
+#
+#        if ProspectoValid and LugarValid:
+#            Lugar = NewLugarForm.save()
+#            Prospecto = NewProspectoForm.save(commit = False)
+#            Prospecto.Direccion = Lugar
+#            Prospecto.save()
+#            return prospecto_lista(request)
+#
+#    context = {
+#        'NewProspectoForm' = NewProspectoForm,
+#        'NewLugarForm' = NewLugarForm,
+#    }
+#    return render(request, 'prospecto/prospecto_form.html', context)
+#
+#def prospecto_lista(request):
+#    context = {}
+#    return render(request, 'prospecto/prospecto_lista.html',context)
+#
