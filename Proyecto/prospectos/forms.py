4ff33dc6ab18808c10b4524a1d9fccc7c1e8be57
lunamrcc <A01209537@itesm.mx>
2018-02-22 20:34:06 -0600

    Update forms.py

---
+++ Proyecto/prospectos/forms.py
@@ -0,0 +1,33 @@
+from .models import Prospecto, Lugar
+from django import forms
+
+class ProspectoForm(forms.ModelForm):
+    class Meta:
+        model = Prospecto
+        fields = (
+            'Nombre',
+            'Apellido_Paterno',
+            'Apellido_Materno',
+            'Telefono_Casa',
+            'Telefono_Celular',
+            'Email',
+            'Metodo_Captacion',
+            'Interes',
+            'EstadoCivil',
+            'Ocupacion',
+            'Hijos',
+        )
+
+class LugarForm(forms.ModelForm):
+    class Meta:
+        model = Lugar
+        fields = (
+            'Calle',
+            'Numero_Interior',
+            'Numero_Exterior',
+            'Colonia',
+            'Ciudad',
+            'Estado',
+            'Pais',
+            'Codigo_Postal',
+        )
