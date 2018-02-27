8ffe0edb75f1a92cd62ebcbd20b05925ad13a9eb
Santiago Valencia <svalenciacorona@gmail.com>
2018-02-26 23:01:35 -0600

    Integrar DataTables e iniciar vista prospectos

--- Proyecto/prospectos/urls.py
+++ Proyecto/prospectos/urls.py
@@ -1,9 +1,9 @@
-from . import views
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import lista_prospecto
 
 app_name = 'prospectos'
 urlpatterns = [
-
+    path('', lista_prospecto, name='lista_prospectos'),
 ] 
