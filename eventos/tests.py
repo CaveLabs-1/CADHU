9225060865d9cfd7ccd37adea8f88a8511e31bc2
Luis <a01701260@itesm.mx>
2018-03-01 00:12:21 -0600

    Tests Finalizados

--- eventos/tests.py
+++ eventos/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from eventos.models import Evento
+from django.urls import reverse
 # Create your tests here.
 
 class EventoModelTest(TestCase):
@@ -13,6 +14,20 @@ class EventoModelTest(TestCase):
         field_label = evento._meta.get_field('Nombre').verbose_name
         self.assertEquals(field_label,'Nombre')
 
+    def test_Descripcion_label(self):
+        evento=Evento.objects.get(id=1)
+        field_label = evento._meta.get_field('Descripcion').verbose_name
+        self.assertEquals(field_label,'Descripcion')
+
+    def test_view_url_exists_at_desired_location(self):
+        resp = self.client.get('/eventos/new')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get('/eventos/new')
+        self.assertEqual(resp.status_code, 200)
+
+        self.assertTemplateUsed(resp, 'eventos/crear_evento.html')
     # def test_get_absolute_url(self):
     #     evento = Evento.objects.get(id=1)
     #     # This will also fail if the urlconf is not defined.
