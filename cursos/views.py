91581e8ba3caea706bb0b9445975264af33e8f44
Mauricio <a01700303@itesm.mx>
2018-03-05 09:14:22 -0600

    validacion forma nuevo curso

--- cursos/views.py
+++ cursos/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Curso
 from eventos.models import Evento
 from django.views import generic
-from .forms import CursosForm
+from .forms import FormaCurso
 
 
 # class CreaCurso(generic.CreateView):
@@ -11,30 +11,49 @@ from .forms import CursosForm
 #     # fields = ['Nombre', 'Fecha', 'Direccion', 'Descripcion', 'Hora', 'Costo', 'Evento']
 #     template_name = 'cursos/nuevo_curso.html'
 
-
 def nuevo_curso(request):
-    NewFormCurso = CursosForm()
+    # NewActividadForm = FormaActividad()
+    Forma_nuevo_curso = FormaCurso()
     if request.method == 'POST':
-        return True;
         # NewActividadForm = FormaActividad(request.POST)
-        # if NewActividadForm.is_valid():
-        #     actividad = NewActividadForm.save()
-        #     return redirect('prospectos:actividades')
-        # context = {
-        #     'form': NewActividadForm,
-        #     'titulo': 'Agregar actividad.',
-        #     'error_message': NewActividadForm.errors
-        # }
-        # return render(request, 'actividades/crear_actividad.html', context)
-        # context = {
-        #     'form': NewActividadForm,
-        #     'titulo': 'Agregar actividad.'
-        # }
-        # eventos = Evento.objects.all()
+        Forma_nuevo_curso = FormaCurso(request.POST)
+        if Forma_nuevo_curso.is_valid():
+            actividad = Forma_nuevo_curso.save()
+            return redirect('/login')
+        else:
+            print ("Error")
+            print (Forma_nuevo_curso.errors)
+        context = {
+            'form': Forma_nuevo_curso,
+            'titulo': 'Agregar curso.',
+            'error_message': Forma_nuevo_curso.errors
+        }
+        return render(request, 'cursos/nuevo_curso.html', context)
     context = {
-        'form': NewFormCurso,
+        'form': Forma_nuevo_curso,
         'titulo': 'Agregar curso.',
         'eventos': Evento.objects.all().order_by('Nombre')
     }
-
     return render(request, 'cursos/nuevo_curso.html', context)
+
+# def nuevo_curso(request):
+#     Forma_Curso = CursosForm()
+#     if request.method == 'POST':
+#         # return True;
+#         Nuevo_Curso = Forma_Curso(request.POST)
+#         if Nuevo_Curso.is_valid():
+#             curso = Nuevo_Curso.save()
+#             return redirect('prospectos:actividades')
+#         context = {
+#             'form': Nuevo_Curso,
+#             'titulo': 'Agregar curso.',
+#             'error_message': Nuevo_Curso.errors
+#         }
+#
+#     context = {
+#         'form': Forma_Curso,
+#         'titulo': 'Agregar curso.',
+#         'eventos': Evento.objects.all().order_by('Nombre')
+#     }
+#
+#     return render(request, 'cursos/nuevo_curso.html', context)
--- static/js/init.js
+++ static/js/init.js
@@ -2,7 +2,29 @@ $(document).ready(function() {
     $('.dataTable').DataTable();
     $('.sidenav').sidenav();
     $('.tooltipped').tooltip({delay: 50});
-    $('.datepicker').datepicker();
     $('.timepicker').timepicker();
     $('select').select();
+
+    tomorrow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+
+    $('.datepicker').datepicker({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15, // Creates a dropdown of 15 years to control year,
+      monthsFull: [ 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre' ],
+      monthsShort: [ 'Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ags', 'Sep', 'Oct', 'Nov', 'Dec' ],
+      weekdaysFull: [ 'Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado' ],
+      weekdaysShort: [ 'Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado' ],
+      weekdaysLetter: [ 'D', 'L', 'M', 'M', 'J', 'V', 'S' ],
+      today: 'Hoy',
+      clear: 'Cerrar',
+      close: 'Ok',
+      format: 'dd/mm/yyyy',
+      formatSubmit: 'dd/mm/yyyy',
+      min: tomorrow,
+      closeOnSelect: false // Close upon selecting a date,
+    });
+
+    console.log('hola');
+
+
 } );
