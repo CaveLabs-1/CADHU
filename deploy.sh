3ceca780df1f8e4ff103c133aa98a695ce42dd12
Marco Mancha <A01206194@itesm.mx>
2018-03-06 17:12:42 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 source my_env/bin/activate
 cd CADHU
-git pull origin develop
+git pull origin master
 pip install -r requirements.txt
 for linea in `cat apps.txt`
 do
   python manage.py makemigrations $linea
   python manage.py migrate
 done
-systemctl restart gunicorn
+sudo systemctl restart gunicorn
+deactivate
+cd ~
