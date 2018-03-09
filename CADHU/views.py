941d847bfe775e140923a16f9b28ec8aeea6a8e8
Marco Mancha <A01206194@itesm.mx>
2018-03-08 19:06:41 -0600

    Terminar documentar tests y código

--- CADHU/views.py
+++ CADHU/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 def redireccionar(request):
+        #Redireccionar la liga principal al login.
         return HttpResponseRedirect(reverse('login'))
