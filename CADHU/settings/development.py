7297570c46f83764590e44f7a5e13c8d2a29ae79
Fily <filyvr1397@gmail.com>
2018-03-09 14:44:47 -0600

    Corregir mi develop

--- CADHU/settings/development.py
+++ CADHU/settings/development.py
@@ -11,20 +11,13 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'cadhu',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.sqlite3',
-'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cadhu',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
 }
