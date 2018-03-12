5d0c67e6ceb6502b8ca03c20ee4ff96ab15503e5
Mauricio <a01700303@itesm.mx>
2018-03-12 09:05:19 -0600

    error messages us26

--- cursos/models.py
+++ cursos/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django import forms
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils import timezone
-# from eventos.models import Evento
 import datetime
 
 class Curso(models.Model):
@@ -11,6 +10,5 @@ class Curso(models.Model):
     Fecha = models.DateField(blank=True, null=True)
     Direccion = models.CharField(max_length=100, blank=True, null=True)
     Descripcion = models.CharField(max_length=150, blank=True, null=True)
-    # Hora = models.TimeField(blank=True, null=True)
     Costo = models.PositiveIntegerField(blank=True, null=True)
     Activo = models.BooleanField(default=True)
