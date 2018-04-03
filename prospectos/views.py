abbf837be0a12d2784d510f73e209274c06d042e
Luis Rodriguez <a01701260@itesm.mx>
2018-04-03 10:34:19 -0500

    Arreglar baja empresa

--- prospectos/views.py
+++ prospectos/views.py
@@ -566,7 +566,7 @@ def crear_empresa(request):
 @group_required('vendedora','administrador')
 def baja_empresas(request, id):
     empresa = Empresa.objects.get(id=id)
-    if Empresa.Activo:
+    if empresa.Activo:
         empresa.Activo = False
         empresa.save()
         return redirect(reverse('prospectos:lista_empresas'))
