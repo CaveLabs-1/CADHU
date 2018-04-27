fd3f6f41ade9cc97dbc2190368b3849bd61a8164
Salmon <a01201954@itesm.mx>
2018-04-27 10:45:32 -0500

    Bulk insert a develop

--- bulk_insert.py
+++ bulk_insert.py
@@ -1,30 +1,33 @@
-#Script para cargar información a la Base de Datos desde archivos .csv
+# Script para cargar información a la Base de Datos desde archivos .csv
 import psycopg2
 
-#Abrir La Conexión de la Base de Datos
+
+# Abrir La Conexión de la Base de Datos
 def abrir_db():
-    #Conexiòn a la Base de Datos
+    # Conexiòn a la Base de Datos
     try:
-        db = psycopg2.connect("host=localhost dbname=cadhu user=cadhu password=8qX8vx1P*Xpu")
+        dtb = psycopg2.connect("host=localhost dbname=cadhu user=cadhu password=8qX8vx1P*Xpu")
         print("CONEXIÓN EXITOSA")
-        return db
+        return dtb
     except psycopg2.Error:
         print('LA CONEXIÓN A LA BASE DE DATOS NO FUE EXITOSA')
 
-#Eliminar Información de Tabla
+
+# Eliminar Información de Tabla
 def eliminar_datos(cursor, tabla):
-    #Ejecutar query para eliminar la información de la tabla
+    # Ejecutar query para eliminar la información de la tabla
     try:
         query = "DELETE FROM " + tabla + ";"
         cursor.execute(query)
         print("DELETE DATA - " + tabla)
-    #En caso de no poder eliminar la información de la tabla
+    # En caso de no poder eliminar la información de la tabla
     except psycopg2.Error:
         print("NO SE PUDO ELIMINAR LA INFORMACIÓN DE LA TABLA " + tabla)
 
-#COPY (BULK INSERT) de un archivo .csv a una tabla
+
+# COPY (BULK INSERT) de un archivo .csv a una tabla
 def subir_archivo(cursor, tabla, path_archivo):
-    #Abrir archivo con la Información
+    # Abrir archivo con la Información
     try:
         with open(path_archivo, 'r') as f:
             # Saltar la primera línea (Titulo Columnas)
@@ -36,15 +39,16 @@ def subir_archivo(cursor, tabla, path_archivo):
             except psycopg2.Error as e:
                 print('NO SE PUDO AGREGAR LA INFORMACIÓN A LA TABLA ' + tabla)
                 print(e.pgerror)
-    #En caso de no poder abrir el archivo
+    # En caso de no poder abrir el archivo
     except IOError:
         print('NO SE PUDO ABRIR ' + path_archivo)
 
-#Eliminar información de todas las tablas
+
+# Eliminar información de todas las tablas
 def borrar_informacion(db):
     print("\n")
     cursor = db.cursor()
-    #Borrar información de la Base de Datos
+    # Borrar información de la Base de Datos
     eliminar_datos(cursor, 'prospectos_pago')
     eliminar_datos(cursor, 'prospectos_cliente')
     eliminar_datos(cursor, 'prospectos_actividad')
@@ -54,29 +58,28 @@ def borrar_informacion(db):
     eliminar_datos(cursor, 'prospectos_empresa')
     eliminar_datos(cursor, 'prospectos_prospecto')
     eliminar_datos(cursor, 'prospectos_lugar')
-    #Guardar cambios en la Base de Datos
+    # Guardar cambios en la Base de Datos
     db.commit()
 
-#Cargar información a las tablas
+
+# Cargar información a las tablas
 def cargar_informacion(db):
     print("\n")
     cursor = db.cursor()
-    #Cargar información de los archivos a las tablas
+    # Cargar información de los archivos a las tablas
     subir_archivo(cursor, 'prospectos_lugar', 'data/Lugar_data.csv')
     subir_archivo(cursor, 'cursos_curso', 'data/Curso_data.csv')
     subir_archivo(cursor, 'grupos_grupo', 'data/Grupo_data.csv')
     subir_archivo(cursor, 'prospectos_empresa', 'data/Empresa_data.csv')
     subir_archivo(cursor, 'prospectos_prospecto', 'data/Prospecto_data.csv')
-    #Guardar cambios en la Base de Datos
+    # Guardar cambios en la Base de Datos
     db.commit()
-
-
 
 
 # Abrir Conexion
 db = abrir_db()
-#borrar_informacion(db)
-#cargar_informacion(db)
-#Cerrar Conexión
+# borrar_informacion(db)
+# cargar_informacion(db)
+# Cerrar Conexión
 if db.close():
     print("CONEXIÓN TERMINADA")
