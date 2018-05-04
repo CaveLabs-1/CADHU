ee4ac129bc4b2b76d6a114c9f8e64a6d38d50347
Santiago Valencia <svalenciacorona@gmail.com>
2018-05-04 11:18:38 -0500

    Arreglar defectos

--- static/js/trigger.js
+++ static/js/trigger.js
@@ -1,5 +1,5 @@
 function whatsapp(celular){
-  window.open("https://web.whatsapp.com/send?phone=" + celular,"_blank")
+  window.open("https://api.whatsapp.com/send?phone=" + celular,"_blank")
 }
 
 // function validarMontoPago(){
